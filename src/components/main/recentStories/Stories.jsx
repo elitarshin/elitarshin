6a29a7ede96c7f22170944d4fe1a173b963c5bbe
elitarshin <ahakobian03storage9@gmail.com>
2025-02-01 01:47:49 +0400

    add new commit

--- src/components/main/recentStories/Stories.jsx
+++ src/components/main/recentStories/Stories.jsx
@@ -10,10 +10,11 @@ const { t } = useTranslation()
 
 return (
     <div className='flex flex-col items-center mt-24'>
-      <h1 className='text-5xl '>{t("recentStories")}</h1>
-      <h1 className='mt-4 text-4xl text-amber-600 mb-10'>{t("dfprj")}</h1>
-      <div className='w-full border-2 p-2 flex justify-center flex-wrap gap-28'>
-        <div className='w-4/5 flex flex-col gap-5 text-center border-2 p-4 max-sm:w-4/5'>
+      <h1 className='text-5xl text-center'>{t("recentStories")}</h1>
+      <h1 className='mt-4 text-4xl text-center text-amber-600 mb-10'>{t("dfprj")}</h1>
+      <div className='w-full p-2 flex justify-center flex-wrap gap-28'>
+        <hr className='w-full h-10'/>
+        <div className='w-3/5 flex flex-col gap-5 text-center border-2 p-4 max-sm:w-4/5'>
             <img src={bgstory} alt="" />
             <h3 className='text-3xl font-bold'>{t("bgstories")}</h3>
             <h4 className="text-xl font-bold">{t("builds")}</h4>
@@ -29,10 +30,10 @@ return (
                 <h3 className='text-xl font-bold'>{t("katlavanstories")}</h3>
             </div>
         </div>
-        <div className='flex flex-col flex-wrap justify-around gap-4 text-center max-md:flex-row flex-wrap max-sm:flex-row flex-wrap'>
-            <div className='w-72 h-40 flex items-center justify-center p-2 border-2 font-bold text-xl max-md:w-2/6 max-sm:w-2/6 text-xs text-center '>{t("apartments")}</div>
-            <div className='w-72 h-40 flex items-center justify-center p-2 border-2 font-bold text-xl max-md:w-2/6 max-sm:w-2/6 text-xs text-center'>{t("twnbuilds")}</div>
-            <div className='w-72 h-40 flex items-center justify-center p-2 border-2 font-bold text-xl flex justify-center max-md:w-2/6 max-sm:w-2/6 text-xs '>{t("earthworks")}</div>
+        <div className='flex flex-wrap items-center justify-around gap-4 text-center mb-10 max-md:flex-row flex-wrap max-sm:flex-col flex-wrap'>
+            <div className='w-72 h-40 flex items-center justify-center p-2 border-2 font-bold text-xl max-md:w-2/6 max-sm:w-5/6 text-xs text-center '>{t("apartments")}</div>
+            <div className='w-72 h-40 flex items-center justify-center p-2 border-2 font-bold text-xl max-md:w-2/6 max-sm:w-5/6 text-xs text-center'>{t("twnbuilds")}</div>
+            <div className='w-72 h-40 flex items-center justify-center p-2 border-2 font-bold text-xl flex justify-center max-md:w-2/6 max-sm:w-5/6 text-xs '>{t("earthworks")}</div>
         </div>
       </div>
 
